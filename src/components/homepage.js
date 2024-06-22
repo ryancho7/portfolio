6acb8c15eee62365323f27830c2146c6ca30a399
ryancho7 <114904773+ryancho7@users.noreply.github.com>
2024-06-21 21:04:30 -0400

    added resume

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -35,7 +35,12 @@ export default function Homepage() {
                 <span className={`hello ${helloLoaded ? 'loaded' : ''}`}>Hello,</span>
                 <span className={`introText ${introTextLoaded ? 'loaded' : ''}`}>I'm <span className="introName">Ryan,</span> <br />A Software Engineer</span>
                 <p className={`introP ${introPLoaded ? 'loaded' : ''}`}>I am a SWE with experience in developing, testing, and maintaining <br />software applications along with creating modern reactive websites</p>
-                <Link><button className={`btn ${isLoaded ? 'loaded' : ''} btn-secondary`}><img src={resume} alt="My Resume" className="btnImg"/> My Resume</button></Link>
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                    <button className={`btn ${isLoaded ? 'loaded' : ''} btn-secondary`}>
+                        <img src={resume} alt="My Resume" className="btnImg"/> My Resume
+                    </button>
+                </a>
+                {/* <Link><button className={`btn ${isLoaded ? 'loaded' : ''} btn-secondary`}><img src={resume} alt="My Resume" className="btnImg"/> My Resume</button></Link> */}
             </div>
             <img src={profileImage} alt="Profile" className={`bg ${isLoaded ? 'loaded' : ''}`}/>
         </section>
